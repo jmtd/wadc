41935df381b0b7124c5c1c064da63cb9386f5da8
Jonathan Dowland <jon+github@alcopop.org>
2015-09-16 02:58:47 +0100

    Remove old utility .batch files

--- launchjdoom.bat
+++
@@ -1,4 +0,0 @@
-c:
-cd \doom2
-bsp \w\wadc\examples\autotex.wad
-jdoomd3d.bat -file tmp.wad -warp 1
--- launchzdoom.bat
+++
@@ -1,6 +0,0 @@
-rem example script, edit to suit your setup
-
-c:
-cd \doom2
-bspz \w\wadc\examples\slopetest.wad
-zdoom.exe -width 512 -file tmp.wad -warp 1
