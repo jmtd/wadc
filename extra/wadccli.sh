75bbe61810c1498bc01f62824a2a089f86a73cb3
Jonathan Dowland <jmtd@debian.org>
2016-09-30 21:50:58 +0100

    Update wadccli example to find the JAR file

--- extra/wadccli.sh
+++ extra/wadccli.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+set -e
+set -u
+
+findjar() {
+	for jar in target/*jar; do
+		if [ -f "$jar" ]; then
+			echo "$jar"
+			return
+		fi
+	done
+	echo /usr/share/wadc/wadc.jar
+}
+JAR=${JAR:-$(findjar)}
+
 # example of how to launch the (alpha) cli
-java -cp wadc.jar org.redmars.wadc.WadCCLI "$@"
+java -cp "$JAR" org.redmars.wadc.WadCCLI "$@"
