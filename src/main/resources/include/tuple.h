7c616e65db4086c95b3e505786c6242aec3e44e6
Jonathan Dowland <jon@dow.land>
2019-01-28 17:55:21 +0000

    add tuple_rotate and tuple_add

--- src/main/resources/include/tuple.h
+++ src/main/resources/include/tuple.h
@@ -56,4 +56,33 @@ tuple_test
     assert(eq(1,tuple_eq(get("t1"),get("t2"))))
     assert(eq(0,tuple_eq(get("t1"),get("t3"))))
     print(tuple_str(get("t1")))
+
+    test_tuple_rotate
+}
+
+tuple_add(t1,t2)
+{
+  tuple(
+  add(fst(t1), fst(t2)),
+  add(snd(t1), snd(t2))
+  )
+}
+
+-- orient has to be strict
+tuple_rotate(t,_orient)
+{
+  ifelse(eq(north, _orient),
+    t,                                         -- North
+    ifelse(eq(east, _orient),
+      tuple(mul(-1,snd(t)), fst(t)),           -- East
+      ifelse(eq(south, _orient),
+        tuple(mul(-1,fst(t)), mul(-1,snd(t))), -- South
+        tuple(snd(t), mul(-1,fst(t))),         -- West
+  )))
+}
+
+test_tuple_rotate
+{
+  assert(tuple_eq(tuple(1,0), tuple_rotate(tuple(1,0), north)))
+  assert(tuple_eq(tuple(0,1), tuple_rotate(tuple(1,0), east)))
 }
